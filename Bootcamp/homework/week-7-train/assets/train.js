7069c4072bc56cb2222900c48e490be35894200b
danielarnost <danielarnost@utexaas.edu>
2016-09-10 10:13:21 -0500

    initial site files

---
+++ Bootcamp/homework/week-7-train/assets/train.js
@@ -0,0 +1,13 @@
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyAO7BFw9UhofcCw2tuWY2CfVCq1wqCnLyk",
+	authDomain: "sdfasdf-d0632.firebaseapp.com",
+	databaseURL: "https://sdfasdf-d0632.firebaseio.com",
+	storageBucket: "sdfasdf-d0632.appspot.com",
+};
+
+firebase.initializeApp(config);
+
+// Create a variable to reference the database
+
+var database = firebase.database();
